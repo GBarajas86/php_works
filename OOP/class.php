46a0759bbf521bb6bd7776f232e3b94237b5ce64
jesus valencia <gualbarajas1986@gmail.com>
2024-11-07 14:13:30 -0600

    adding encapsulation

--- OOP/class.php
+++ OOP/class.php
@@ -1,24 +1,41 @@
 <?php
-$sale = new Sale(10.5, date("y-m-d"));
+
+declare(strict_types=1);
+
+$sale = new Sale(date("y-m-d"));
 $concept = new Concept("Cerveza", 10);
+$concept2 = new Concept("Botanas", 40);
+$sale->addConcepts($concept2);
 $sale->addConcepts($concept);
-print_r($sale->concepts);
+echo $sale->getTotal();
+
+
+$onlineSale = new OnlineSale(date("y-m-d"), "debit card");
+// echo $onlineSale->createInvoice();
+// echo $onlineSale->showInfo();
+
+
 
 class Sale{
-    public float $total;
+    protected float $total;
     public string $date;
-    public array $concepts;
+    private array $concepts;
     public static $count = 0;
 
-    public function __construct(float $total, string $date) {
-        $this->total = $total;
+    public function __construct(string $date) {
+        $this->total = 0;
         $this->date = $date;
         $this->concepts = [];
         self::$count++;
     }
 
-    public function addConcepts(Concept $concepts)  {
-        $this->concepts[] = $concepts;
+    public function addConcepts(Concept $concept)  {
+        $this->concepts[] = $concept;
+        $this->total += $concept->amount;
+    }
+
+    public function getTotal(): float {
+        return $this->total;
     }
 
     public static function reset(){
@@ -32,7 +49,20 @@ class Sale{
 
 }
 
-Class Concept {
+class OnlineSale extends Sale {
+    public $paymentMethod;
+
+    public function __construct(string $date, string $paymentMethod) {
+        parent::__construct($date);
+        $this->paymentMethod = $paymentMethod;
+    }
+
+    function showInfo() : string {
+        return "La venta tiene un monto de $this->total";
+    }
+}
+
+class Concept {
     public string $description;
     public float|int $amount;
 
